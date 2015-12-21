9358c1eff6ba6e31c055d1023bd98b86956decc4
Léo <leonardo_carvalho@outlook.com>
2015-12-21 10:25:09 -0200

    UPDATE

--- install-development-programs.sh
+++ install-development-programs.sh
@@ -15,13 +15,19 @@ show_options()
 	echo "================================================================================"
 	echo "FERRAMENTAS PARA DESENVOLVIMENTO"
 	echo "================================================================================"
-	echo "[1]ECLIPSE             [2]NETBEANS        [9]PHP"
-	echo "[3]SUBLIME TEXT        [4]PHPSTORM        [10]MYSQL"
-	echo "[5]ANDROID STUDIO      [6]ASTAH           [11]RUBY/SASS"
-	echo "[7]NOTEPADQQ           [8]APACHE          [12]GIT"
+	echo "[1]ECLIPSE             [5]NETBEANS        [9]PHP"
+	echo "[2]SUBLIME TEXT        [6]PHPSTORM        [10]MYSQL"
+	echo "[3]ANDROID STUDIO      [7]ASTAH           [11]RUBY/SASS"
+	echo "[4]NOTEPADQQ           [8]APACHE          [12]GIT"
 	echo "================================================================================"
 	
 }
+
+install_eclipse()
+{
+    
+}
+
 
 
 
--- install-essentials-programs.sh
+++ install-essentials-programs.sh
@@ -10,3 +10,32 @@ is_root()
 	fi
 }
 
+show_options()
+{
+	echo "================================================================================"
+	echo "PROGRAMAS ESSENCIAIS"
+	echo "================================================================================"
+	echo "[1]Spotify            [5]UNITY TWEAK TOOL"
+	echo "[2]Telegram"
+	echo "[3]VirtualBox"
+	echo "[4]VLC"
+	echo "================================================================================"
+	
+}
+
+install_spotify()
+{
+    sh -c "echo 'deb http://repository.spotify.com stable non-free' >> /etc/apt/sources.list"
+    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D2C19886
+    apt-get update
+    apt-get install spotify-client
+    show_options
+}
+
+install_telegram()
+{
+	add-apt-repository ppa:costales/unity-webapps-telegram
+	apt-get update
+	apt-get install unity-webapps-telegram
+	show_options
+}
